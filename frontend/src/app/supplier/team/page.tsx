499da18d7d8eebdd948190d6d9de4d53d4d98f4e
ChamodiSandunika <chamodisandunika2002@gmail.com>
2025-08-19 23:41:53 +0530

    feat: create TeamPage component with SupplierLayout and TeamManagement

---
+++ frontend/src/app/supplier/team/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import SupplierLayout from '../../../components/layout/SupplierLayout';
+import TeamManagement from '../../../components/supplier/TeamManagement';
+
+export default function TeamPage() {
+  return (
+    <SupplierLayout>
+      <TeamManagement />
+    </SupplierLayout>
+  );
+}
