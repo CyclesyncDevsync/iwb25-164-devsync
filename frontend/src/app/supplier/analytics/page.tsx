c4d12949172d4472ef38ab767167699fd373ade5
ChamodiSandunika <chamodisandunika2002@gmail.com>
2025-08-19 23:43:40 +0530

    feat: add AnalyticsPage component with SupplierLayout and SupplierAnalytics

---
+++ frontend/src/app/supplier/analytics/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import SupplierLayout from '../../../components/layout/SupplierLayout';
+import SupplierAnalytics from '../../../components/supplier/SupplierAnalytics';
+
+export default function AnalyticsPage() {
+  return (
+    <SupplierLayout>
+      <SupplierAnalytics />
+    </SupplierLayout>
+  );
+}
