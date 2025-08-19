e7a73ddf5b25cc64df632d9d4276e59eedffd6d5
ChamodiSandunika <chamodisandunika2002@gmail.com>
2025-08-19 23:42:13 +0530

    feat: add NotificationsPage component with SupplierLayout and NotificationCenter

---
+++ frontend/src/app/supplier/notifications/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import SupplierLayout from '../../../components/layout/SupplierLayout';
+import NotificationCenter from '../../../components/supplier/NotificationCenter';
+
+export default function NotificationsPage() {
+  return (
+    <SupplierLayout>
+      <NotificationCenter />
+    </SupplierLayout>
+  );
+}
